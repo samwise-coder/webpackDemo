ca0018b480516e96fd6616fcfc043aa90217bfc7
samwise-coder <houguochao.outlook.com>
2021-09-14 23:24:20 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    foo() {
+      console.log("webpack配置需要和webpackChain配合使用！");
+    },
+  },
+};
 </script>
 
 <style>
